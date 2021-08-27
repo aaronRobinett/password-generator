71f7437935b1a31d6be7c4824d2ea42b066c3d11
Aaron <aaronrobinett@gmail.com>
2021-08-27 01:31:58 -0700

    functionality complete

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,4 @@
 // Assignment code here
-/////////////////////////////////////////////////////////////////////////////////////////////////
-
 var generatePassword = function () {
   var userChoices = getUserInput();
   return makePassword(userChoices);
@@ -46,83 +44,31 @@ var getUserInput = function () {
 }
 
 var makePassword = function (choices) {
+  var pwd = "";
+  charOptions = [];
+  if (choices[1] === 1) {
+    charOptions.push("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z");
+  }
+  if (choices[2] === 1) {
+    charOptions.push("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z");
+  }
+  if (choices[3] === 1) {
+    charOptions.push("0", "1", "2", "3", "4", "5", "6", "7", "8", "9");
+  }
+  if (choices[4] === 1) {
+    charOptions.push(" ", "!", "\"", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~");
+  }
+  for (var i = 0; i < choices[0]; i++) {
+    pwd = pwd + randomEntry(charOptions);
+  }
 
-  return "fuck"
-
+  return pwd
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/////////////////////////////////////////////////////////////////////////////////////
-
-
+var randomEntry = function (array) {
+  var index = Math.floor(Math.random() * array.length);
+  return array[index];
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
