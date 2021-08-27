abc1dfa2a2fae1fc92bac0afe98321460505c7b0
Aaron <aaronrobinett@gmail.com>
2021-08-27 00:28:48 -0700

    gets user input

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,127 @@
 // Assignment code here
+/////////////////////////////////////////////////////////////////////////////////////////////////
+
+var generatePassword = function () {
+  var userChoices = getUserInput();
+  return makePassword(userChoices);
+}
+
+var getUserInput = function () {
+
+  var lengthPrompt = 0;
+  while (lengthPrompt < 8 || lengthPrompt > 128) {
+
+    lengthPrompt = Number(window.prompt("How long would you like your password to be? (please enter a number between 8 and 128)"));
+    if (isNaN(lengthPrompt)) {
+      lengthPrompt = 0;
+    }
+  }
+
+  var charTypes = ["lowercase", "uppercase", "numeric", "special"];
+  var typeChoices = [0, 0, 0, 0];
+  var choiceCount = 0;
+
+  while (choiceCount === 0) {
+    for (var i = 0; i < charTypes.length; i++) {
+      var typePrompt = window.prompt("Would you like to include " + charTypes[i] + " characters in your password? \nType YES or NO");
+
+      switch (typePrompt.toLowerCase()) {
+        case "yes":
+          typeChoices[i] = 1;
+          choiceCount++;
+          break;
+        case "no":
+          break;
+        default:
+          window.alert("Please enter 'YES' or 'NO'");
+          i--;
+      }
+    }
+    if (choiceCount === 0) {
+      window.alert("You must choose at lease one character type for your password. Please try again.");
+    }
+  }
+  var choiceArray = [lengthPrompt, typeChoices[0], typeChoices[1], typeChoices[2], typeChoices[3]];
+  return choiceArray;
+}
+
+var makePassword = function (choices) {
+
+  return "fuck"
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/////////////////////////////////////////////////////////////////////////////////////
+
 
 
 // Get references to the #generate element
